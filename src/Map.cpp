5ee89b90537a09d24d75bac626069c580d195ec4
Dan Joar Heimvik <danjoar@gmail.com>
2023-11-07 11:13:15 +0100

    Changed map to store different entities

--- src/Map.cpp
+++ src/Map.cpp
@@ -7,7 +7,7 @@
 
 void Map::addObstacle(int x, int y) {
     if(isValidCell(x, y)) {
-        grid[y][x] = true;
+        grid[y][x] = OBSTACLE;
     }
 }
 
@@ -19,7 +19,7 @@ void Map::removeObstacle(int x, int y) {
 
 bool Map::hasObstacle(int x, int y) {
     if(isValidCell(x, y)) {
-        return grid[y][x];
+        return grid[y][x] == OBSTACLE;
     }
     return false;
 }
@@ -41,5 +41,35 @@ void Map::setObstacles(const std::vector<Point>& obstacleNodes) {
         int x = node.x;
         int y = node.y;
         addObstacle(x, y);
+    }
+}
+
+void Map::setRobotPosition(int x, int y) {
+    if(isValidCell(x, y)) {
+        grid[y][x] = ROBOT;
+    }
+}
+
+void Map::setTaskPosition(int x, int y) {
+    if(isValidCell(x, y)) {
+        grid[y][x] = TASK;
+    }
+}
+
+void Map::displayMap() const {
+    for (int i = 0; i < height; i++) {
+        printf("|");
+        for (int j = 0; j < width; j++) {
+            if (grid[i][j] == EMPTY) {
+                printf(" ");
+            } else if (grid[i][j] == OBSTACLE) {
+                printf("O");
+            } else if (grid[i][j] == ROBOT) {
+                printf("R");
+            } else if (grid[i][j] == TASK) {
+                printf("T");
+            }
+        }
+        printf("|\n");
     }
 }
--- src/Map.h
+++ src/Map.h
@@ -13,22 +13,38 @@
 
 class Map {
 public:
+
+    // Constants for map entities
+    static const int EMPTY = 0;
+    static const int OBSTACLE = 1;
+    static const int ROBOT = 2;
+    static const int TASK = 3;
+
     Map() = default;
     Map(int width, int height) : width(width), height(height) {
-        grid = std::vector<std::vector<bool>>(height, std::vector<bool>(width, false));
-
+        grid.resize(height);
+        for (int i = 0; i < height; i++) {
+            grid[i].resize(width);
+            for (int j = 0; j < width; j++) {
+                grid[i][j] = EMPTY;
+            }
+        }
     }
 
     void addObstacle(int x, int y);
     void removeObstacle(int x, int y);
     bool hasObstacle(int x, int y);
     void setObstacles(const std::vector<Point>& obstacleNodes);
+    void setRobotPosition(int x, int y);
+    void setTaskPosition(int x, int y);
 
     int getWidth() const;
     int getHeight() const;
 
+    void displayMap() const;
+
 private:
-    std::vector<std::vector<bool>> grid;
+    std::vector<std::vector<int>> grid;
     int width;
     int height;
 
