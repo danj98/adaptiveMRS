ddde909565e297cbafa2f77058396e28e0519a79
Dan Joar Heimvik <danjoar@gmail.com>
2023-11-17 10:48:52 +0100

    Prints map with path

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "point.h"
 #include "Robot.h"
 #include "Map.h"
+#include "GlobalMissionManager.h"
 
 
 void assignTasks(YAML::Node mission, YAML::Node robotInfo) {
@@ -34,11 +35,11 @@ int main(int argc, char* argv[]) {
 
     Map map = Map(width, height);
 
-    std::vector<Point> obstacles;
+    std::vector<Cell> obstacles;
     for (const auto& node : obstacleNodes) {
         int x = node[0].as<int>();
         int y = node[1].as<int>();
-        obstacles.emplace_back(x, y);
+        obstacles.emplace_back(x, y, CellType::OBSTACLE);
     }
 
     map.setObstacles(obstacles);
@@ -47,14 +48,14 @@ int main(int argc, char* argv[]) {
     Robot robot1 = Robot(robotInfo[0]["name"].as<std::string>(),
                          robotInfo[0]["id"].as<int>(),
                          robotInfo[0]["battery"].as<int>(),
-                        Point(robotInfo[0]["start_position"][0].as<int>(), robotInfo[0]["start_position"][1].as<int>()),
+                        Cell(robotInfo[0]["start_position"][0].as<int>(), robotInfo[0]["start_position"][1].as<int>(), CellType::ROBOT),
                          robotInfo[0]["active"].as<bool>(),
                          robotInfo[0]["capabilities"].as<int>());
 
     Robot robot2 = Robot(robotInfo[1]["name"].as<std::string>(),
                          robotInfo[1]["id"].as<int>(),
                          robotInfo[1]["battery"].as<int>(),
-                         Point(robotInfo[1]["start_position"][0].as<int>(), robotInfo[1]["start_position"][1].as<int>()),
+                         Cell(robotInfo[1]["start_position"][0].as<int>(), robotInfo[1]["start_position"][1].as<int>(), CellType::ROBOT),
                          robotInfo[1]["active"].as<bool>(),
                          robotInfo[1]["capabilities"].as<int>());
 
@@ -65,11 +66,28 @@ int main(int argc, char* argv[]) {
     map.setRobotPosition(robot2.getPosition().x, robot2.getPosition().y);
 
     // Task creation
-    Point goal = Point(mission["goal_position"][0].as<int>(), mission["goal_position"][1].as<int>());
+    Cell goal = Cell(mission["goal_position"][0].as<int>(), mission["goal_position"][1].as<int>(), CellType::TASK);
     map.setTaskPosition(goal.x, goal.y);
+
+    Mission m = Mission(std::vector<Task>{}, Task(goal.x, goal.y, 0, "goal", 1));
 
     // Draw map
     map.displayMap();
+
+    std::vector<Robot> robots;
+    robots.push_back(robot1);
+    robots.push_back(robot2);
+    GlobalMissionManager globalMissionManager = GlobalMissionManager(robots, m, map);
+
+    BidResult bid = globalMissionManager.getHighestBid(robots, m, map);
+    std::cout << "\n";
+    map.displayMapWithRoute(bid.selectedRobot.getRoute());
+
+    // Print selected route
+    std::cout << "Route: ";
+    for (const auto& cell : bid.selectedRobot.getRoute()) {
+        std::cout << "[" << cell.x << ", " << cell.y << "] ";
+    }
 
 
     return 0;
