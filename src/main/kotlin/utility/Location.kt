ad51d8d4bfad4b36c3c1592708e4b0d6f792ae8f
Dan Joar Heimvik <danjoar@gmail.com>
2024-02-20 13:05:48 +0100

    Refactor

--- src/main/kotlin/utility/Location.kt
+++ src/main/kotlin/utility/Location.kt
@@ -3,21 +3,11 @@ package adaptiveMRS.utility
 import adaptiveMRS.mission.Context
 
 data class Location(
-    val x: Int,
-    val y: Int
+    val x: Int = 0,
+    val y: Int = 0
 ) {
     fun distanceTo(other: Location, context: Context): Int {
-        return aStar(this, other, context).size
-    }
-
-    fun isObstacle(context: Context): Boolean {
-        for (obstacle in context.obstacles) {
-            if (x >= obstacle.shell[0].x && x <= obstacle.shell[2].x) {
-                if (y >= obstacle.shell[0].y && y <= obstacle.shell[2].y) {
-                    return true
-                }
-            }
-        }
-        return false
+        val distance = aStar(this, other, context).size
+        return distance
     }
 }
